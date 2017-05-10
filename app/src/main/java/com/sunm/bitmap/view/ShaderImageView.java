44d5dde179870ee897c6dfa4d201a16cceedf572
Sun Meng <sunmeng@apusapps.com>
2017-05-10 21:25:28 +0800

    push code

--- app/src/main/java/com/sunm/bitmap/view/ShaderImageView.java
+++ app/src/main/java/com/sunm/bitmap/view/ShaderImageView.java
@@ -15,7 +15,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
